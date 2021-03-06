8ea79f642b24b9b10d7f2f8359f0a75f31934572
Jon Santos <jonsantox@gmail.com>
2021-03-06 16:20:21 +0100

    COlumns route added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const chalk = require('chalk');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var columnsRouter = require('./routes/columns');
 
 const cors = require('cors');
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/columns', columnsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
