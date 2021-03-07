81c7e966ce00cda3aec1f984339a528a0ac741e1
Santoxjon <jonsantox@gmail.com>
2021-03-07 16:14:10 +0100

    Task edition

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,7 +8,7 @@ router.use(express.urlencoded({ extended: false }));
 
 /* GET home page. */
 router.post('/create', function (req, res, next) {
-    let id = new Date().getTime();
+    let id = parseInt(new Date().getTime());
     let name = req.body.name;
     let tasktext = req.body.tasktext;
     let priority = +req.body.priority;
@@ -25,5 +25,67 @@ router.post('/create', function (req, res, next) {
         }
     });
 });
+
+router.get('/:column_id/:task_id', function (req, res, next) {
+    const _id = new ObjectId(req.params.column_id);
+    const id = req.params.task_id;
+
+    req.app.locals.db.collection("columns").findOne({ _id }, function (err, data) {
+        if (err != null) {
+            console.log(err);
+            res.send({ mensaje: "error: " + err });
+        } else {
+            res.json(data)
+        }
+    });
+});
+
+router.put('/update', function (req, res, next) {
+    let _id = new ObjectId(req.body.columnId);
+    let id = req.body.taskId;
+    let name = req.body.name;
+    let tasktext = req.body.tasktext;
+    let priority = req.body.priority;
+
+    req.app.locals.db.collection("columns").aggregate(
+        [
+            {
+                $match: { _id }
+            },
+            {
+                $project:
+                {
+                    index: { $indexOfArray: ["$tasks.id", id] },
+                }
+            }
+        ]
+    ).toArray(
+        function (err, data) {
+            if (err != null) {
+                console.log(err);
+                res.send({ mensaje: "error: " + err });
+            } else {
+                req.app.locals.db.collection("columns").updateOne(
+                    { _id },
+                    {
+                        $set: {
+                            [`tasks.${data[0].index}.name`]: name,
+                            [`tasks.${data[0].index}.tasktext`]: tasktext,
+                            [`tasks.${data[0].index}.priority`]: priority,
+                        }
+                    },
+                    function (err) {
+                        if (err != null) {
+                            console.log(err);
+                            res.send({ mensaje: "error: " + err });
+                        } else {
+                            res.json('Exito');
+                        }
+                    }
+                )
+            }
+        })
+});
+
 
 module.exports = router;
